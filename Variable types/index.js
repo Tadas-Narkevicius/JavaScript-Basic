6d8d5a3d95f4c0ec3186a0d0e28e2e9aba881179
Tadas Narkevicius <tadass411@gmail.com>
2019-01-30 14:03:39 +0200

    add all files

---
+++ Variable types/index.js
@@ -0,0 +1,23 @@
+
+// Strings
+var myVar2 = "Hello";
+var name = 'Jonas';
+
+var myString = 'I\'m a "fun" string';
+
+// Number
+var number = 5;
+var decimal = 3.00; 
+
+// Constanta
+const PI = 3.14;
+
+// Booleans
+var myVar = true;
+var myVar = false;
+
+// Array
+var cars = ["Saab", "Volvo", "BMW"]; 
+
+// Object
+var John = { firstName:"John", lastName:"Doe"}; 
